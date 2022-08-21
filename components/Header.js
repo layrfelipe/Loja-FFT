ffb3659fd21fda37a830e39afa56a32f7cddc58c
layrfelipe <layrfpf@gmail.com>
2022-08-21 19:02:51 -0300

    teste 1 menu mobile

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import { useState } from "react";
 
 import styles from '../styles/Header.module.scss';
 import logo from '../public/images/logos/logo-brasao.png'
@@ -10,31 +11,48 @@ export default function Header() {
 
     const router = useRouter();
 
+    const [open, setOpen] = useState(false);
+    const handleClick = () => {
+        setOpen(!open)
+        console.log(open)
+    }
+
     return (
         <div className={styles.headerContainer}>
-            <Link href='/'>
-                <a>
-                    <div className={styles.logoAndTextContainer}>
-                        <div className={styles.logoContainer}>
-                            <Image
-                                src={logo}
-                                alt='EDITAR ALT'
-                            />
+            <div className={styles.headerBar}>
+                <Link href='/'>
+                    <a>
+                        <div className={styles.logoAndTextContainer}>
+                            <div className={styles.logoContainer}>
+                                <Image
+                                    src={logo}
+                                    alt='EDITAR ALT'
+                                />
+                            </div>
+                            <p>FANFARRA<br />FESTIVA<br />TRICOLOR</p>
                         </div>
-                        <p>FANFARRA<br />FESTIVA<br />TRICOLOR</p>
-                    </div>
-                </a>
-            </Link>
+                    </a>
+                </Link>
 
-            <ul className={styles.menu}>
-                <Link href='/'><li className={router.pathname == '/' ? styles.active : styles.inactive}>Início</li></Link>
-                <Link href='/loja'><li className={router.pathname == '/loja' ? styles.active : styles.inactive}>Loja</li></Link>
-                <Link href='/mart'><li className={router.pathname == '/mart' ? styles.active : styles.inactive}>mArt</li></Link>
-                <Link href='/sobre-nos'><li className={router.pathname == '/sobre-nos' ? styles.active : styles.inactive}>Sobre nós</li></Link>
-            </ul>
+                <ul className={styles.menu}>
+                    <Link href='/'><li className={router.pathname == '/' ? styles.active : styles.inactive}>Início</li></Link>
+                    <Link href='/loja'><li className={router.pathname == '/loja' ? styles.active : styles.inactive}>Loja</li></Link>
+                    <Link href='/mart'><li className={router.pathname == '/mart' ? styles.active : styles.inactive}>mArt</li></Link>
+                    <Link href='/sobre-nos'><li className={router.pathname == '/sobre-nos' ? styles.active : styles.inactive}>Sobre nós</li></Link>
+                </ul>
 
-            <div className={styles.buttonContainer}>
-                <Image src={burger} alt="EDITAR ALT" layout='responsive'/>
+                <div className={styles.buttonContainer} onClick={handleClick}>
+                    <Image src={burger} alt="EDITAR ALT" layout='responsive'/>
+                </div>
+            </div>
+
+            <div className={open ? styles.burgerMenuOpen : styles.burgerMenuClosed}>
+                <ul>
+                    <li><Link href='/'>Início</Link></li>
+                    <li><Link href='/loja'>Loja</Link></li>
+                    <li><Link href='/mart'>mArt</Link></li>
+                    <li><Link href='/sobre-nos'>Sobre nós</Link></li>
+                </ul>
             </div>
         </div>
     )
